376d451fa36d412476490a4cd1607b4f573de1e0
eladgo123 <eladgolan8@gmail.com>
2022-12-11 22:41:21 +0200

    PasswordNew

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Pas from './Pas' ; 
+import { useState } from 'react';
 
 function App() {
+    const[password,setpassword] = useState(1)
+    const[btn , setbtn] = useState(true)
+    
+    function hendelPassword(e){
+        //console.log(e)
+        setpassword/*(Number*/(e.target.value)/*)*/
+    }
+   function disableBtn(x){
+        setbtn(x)
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Password App</h1>
+      <input type ="string" value={password} onChange={hendelPassword}/> 
+      <button disabled={btn}>Submit</button>
+      <Pas password={password} disableBtn={disableBtn}/>
     </div>
   );
 }
-
 export default App;
---
+++ src/Pas.js
@@ -0,0 +1,29 @@
+import { useState , useEffect } from 'react';
+
+function Pas(props) {
+    const {password , disableBtn } = props
+    const[strength , setstrength ] = useState("WEAK")
+    
+    useEffect(()=>{
+      console.log(password.length)
+      if(password.length > 2 && password.length < 7){
+        setstrength("Medium")
+        disableBtn(false)
+      }
+      if (password.length > 6) {
+          setstrength("Big")
+          disableBtn(false)
+      } else {
+        setstrength("WEAK")
+      }
+    },[password,disableBtn])
+    
+
+     return (
+       <div >
+         <h1>The password is {strength}</h1>
+       </div>
+     );
+   }
+
+export default Pas;
